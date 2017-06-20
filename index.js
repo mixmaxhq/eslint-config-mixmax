b8c8e559ffe2d957ba6a2c1b4488fd5b7d7dcf05
Eli Skeggs <skeggse@users.noreply.github.com>
2017-06-19 19:04:36 -0700

    Add common additional rules

--- index.js
+++ index.js
@@ -8,6 +8,26 @@ module.exports = {
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
-    'no-use-before-define': ['error', 'nofunc']
+    'no-use-before-define': ['error', 'nofunc'],
+    'no-unused-vars': ['error', {
+      // Per https://github.com/eslint/eslint/issues/1494#issuecomment-232890308.
+      'argsIgnorePattern': 'next'
+    }],
+    quotes: ['error', 'single', {
+      // Let people use strings like "don't worry about single quotes".
+      'avoidEscape': true
+    }],
+    // Enforce our whitespace styles around keywords.
+    'keyword-spacing': ['error', {
+      'overrides': {
+        'function': {
+          'after': false
+        }
+      }
+    }],
+    // Console logs should be mostly nonexistent, and places like app.js should just have a 
+    // /* eslint-disable no-console */
+    // comment at the top.
+    'no-console': ['error'],
   }
 };
