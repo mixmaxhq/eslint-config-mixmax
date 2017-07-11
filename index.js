7299d0731f638dd6423ca2912198e8cb942b4722
Spencer Brown <spencer@mixmax.com>
2017-07-11 12:43:57 -0700

    Add setting for indentation in `switch` `case`s

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ module.exports = {
   },
   extends: 'eslint:recommended',
   rules: {
-    indent: ['error', 2],
+    indent: ['error', 2, {
+      SwitchCase: 1
+    }],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
