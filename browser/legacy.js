eef0a971d874657da68e47fd18f8b1aad4308de0
Eli Skeggs <eli@mixmax.com>
2018-08-22 15:59:16 -0700

    Deprecate using global browser libraries

---
+++ browser/legacy.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'mixmax/browser',
+  env: {
+    // TODO: We should not globalize this - we should explicitly import our CDN-sourced dependencies.
+    jquery: true,
+  },
+  globals: {
+    // TODO: We should not globalize this - we should explicitly import our CDN-sourced dependencies.
+    Backbone: true,
+  },
+};
